03890dd1f166d1871784976468815b2bcbca9fc7
v-jishnu <v.jishnu2005@gmail.com>
2025-07-03 21:39:47 +0530

    backend folder created

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT||3000,() => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+  })
+  .catch( (err) => {
+    console.error('Database connection error:', err);
+  });  
